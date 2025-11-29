af0ea0e05cd7af209672883b36c90a2099f45f0a
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-11-29 13:02:22 +0100

    create new stuff

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lanng="ts" setup>
+import { ref } from "vue"; 
+  const message = ref("Tasks App");
+  const newTask = ref("");
+
 </script>
 
+
+
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <h1>{{message}}</h1>  
+    <from>
+    <label>
+      New Task:
+      <input v-model="newTask" name="newTask"/>
+    </label>
+
+    <div class="button-container">
+      <button>Add</button>
+    </div>
+    
+    </from>
+    <h3>{{newTask}}</h3>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+main {
+  max-width: 800px;
+  margin: 1rem auto;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.button-container {
+  display: flex;
+  justify-content: end;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
