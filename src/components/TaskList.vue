970ef74fc921a5179a881c3b185e06166fc64397
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-01 13:03:40 +0100

    added the toggleDone to eimt

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -4,9 +4,24 @@ const props = defineProps<{
     tasks: Task[]
 }>();
 
+const emit = defineEmits<{
+    toggleDone: [id: string]
+}>();
+
 </script>
 <template>
-    <article v-for="task in props.tasks" :key="task.id">
-        {{ task.title }}
-    </article>
+    <div class="task-list">
+        <article v-for="task in props.tasks" :key="task.id">
+            <label>
+                <input @input="emit('toggleDone', task.id)" :checked="task.done" type="checkbox">
+            </label>
+            {{ task.title }}
+        </article>
+    </div>
 </template>
+
+<style>
+.task-list{
+    margin-top: 1rem;
+}
+</style>
