12e951cfebf09b9ea13317bae0beda724a99279d
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-01 13:18:42 +0100

    created total done value to display them

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import TaskForm from './components/TaskForm.vue'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import type { Task } from './types';
 import TaskList from './components/TaskList.vue';
 
@@ -8,6 +8,10 @@ const emit = defineEmits<{ addTask: [task: string] }>()
 
 const message = ref('Welcome to the Task Manager App!')
 const tasks = ref<Task[]>([])
+
+const totalDone = computed(() => tasks
+.value
+.reduce((total, task) => task.done ? total + 1 : total, 0))
 
 function addTask(newTask: string) {
   tasks.value.push({
@@ -31,7 +35,7 @@ function toggelDone(id: string){
     <h1>{{ message }}</h1>
     <TaskForm @addTask="addTask"/>
     <h3 v-if="!tasks.length"> Add a Task to Get started.</h3>
-    <h3 v-else>0 / {{ tasks.length }} tasks comleted</h3>
+    <h3 v-else>{{ totalDone }} / {{ tasks.length }} tasks comleted</h3>
     <TaskList :tasks @toggle-done="toggelDone"/>
 </main>
 </template>
