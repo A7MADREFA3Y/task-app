8bc03c1541b69070ce6b223f298d639ea5a3fd14
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-01 11:36:21 +0100

    extract TaskForm form the app file the components

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const emit = defineEmits<{
+  addTask: [newTask: string]
+}>()
+
+const newTask = ref("")
+
+function formSubmetted() {
+  if (newTask.value.trim()) {
+    emit("addTask", newTask.value)
+    newTask.value = ""  
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="formSubmetted">
+    <label>
+      New Task:
+      <input v-model="newTask" name="newTask" />
+    </label>
+    <div class="button-container">
+      <button type="submit">Add</button>
+    </div>
+  </form>
+</template>
