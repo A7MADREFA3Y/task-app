eb4ca479a9258ae2536b5d5cc1973ce9f4b07c72
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-02 10:41:20 +0100

    final touches on the button

--- src/components/FilterButton.vue
+++ src/components/FilterButton.vue
@@ -1,23 +1,30 @@
 <script lang="ts" setup>
 import type { TaskFilter } from '../types';
 
-
 const props = defineProps<{
-    filter : TaskFilter
+  filter: TaskFilter;
+  currentFilter: TaskFilter;
 }>();
 
 const emits = defineEmits<{
-    setFilter :[filter: TaskFilter]
+  setFilter: [filter: TaskFilter]
 }>();
 </script>
 
 <template>
-    <button class="secondary" @click="emits('setFilter', props.filter)">{{ props.filter }}</button>
-
+  <button 
+    class="secondary"
+    @click="emits('setFilter', props.filter)"
+    :class="{
+      contrast: props.currentFilter === props.filter
+    }"
+  >
+    {{ props.filter }}
+  </button>
 </template>
 
 <style scoped>
-button{
-    text-transform: capitalize;
+button {
+  text-transform: capitalize;
 }
 </style>
